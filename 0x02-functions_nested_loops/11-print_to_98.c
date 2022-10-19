fcc586309c98c1cd3e075e2a8106e8c80ab879ee
D1N2O3B4 <davebostons2017@gmail.com>
2022-10-19 07:36:36 -0700

    hot fix

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_to_98 - prints from a given number upward till 98
+ * @n: is an int
+ * Return: Always 0
+ */
+void print_to_98(int n)
+{
+  for (a = n; a < 98; a++)
+    {
+      _putchar(a);
+    }
+}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,12 +6,17 @@
  */
 int print_last_digit(int i)
 {
+int a;
 if (i < 0)
 {
-int y;
-i = -i;
-y = i % 10;
-_putchar(y + '0');
+i = i * -1;
+a = i % 10;
+_putchar(a + '0');
 }
-return (y);
+else
+{
+a = i % 10;
+_putchar(a + '0');
+}
+return (a);
 }
