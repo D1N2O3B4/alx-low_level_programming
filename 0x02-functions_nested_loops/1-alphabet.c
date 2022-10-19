bd2e21cc3c4475b966271718e3c559fb7144f111
D1N2O3B4 <davebostons2017@gmail.com>
2022-10-19 05:33:43 -0700

    modified 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,8 @@
 #include "main.h"
 /**
- * main - 
+ * print_alphabet -> prints lower case alphabets
  * Return: Always 0
  */
-void print_alphabet(void);
-int main(void)
-{
-print_alphabet();
-return (0);
-}
 void print_alphabet(void)
 {
 char alp [26] = "abcdefghijklmnopqrstuvwxyz";
