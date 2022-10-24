93ff20c6d6dfc7a096af231cf19bf8e5d6199e62
D1N2O3B4 <davebostons2017@gmail.com>
2022-10-24 08:34:06 -0700

    added task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * reset_to_98() - pointer function update to 98
- * @n - is a pointer to an integer
+ * reset_to_98 - pointer function update to 98
+ * @n: is a pointer to an integer
  */
 void reset_to_98(int *n)
 {
