653d768069ac3de81dd80456f0a3b254f3d5e28e
D1N2O3B4 <davebostons2017@gmail.com>
2022-10-24 13:09:44 -0700

    added 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,8 +1,22 @@
 #include "main.h"
 /**
  * puts2 - print string new line of to the next
- * @str - is a char
+ * @str: is a char
  */
 void puts2(char *str)
 {
+int i = 0;
+while (str[i] != '\0')
+{
+if ((i % 2) == 0)
+{
+_putchar(str[i]);
+}
+else
+{
+continue;
+}
+i = i + 1;
+}
+_putchar('\n');
 }
